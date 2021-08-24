10fbe3a5e142fff24cb68df71276cbf2e9d883c0
tksmasaki <tk.masaki70@gmail.com>
2021-08-24 13:51:14 +0900

    日本語、英語対応機能を追加

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,17 @@
 # frozen_string_literal: true
 
 class ApplicationController < ActionController::Base
+  around_action :switch_locale
+
+  def default_url_options
+    # default_localeと等しい時は、ヘルパーメソッドで生成するurlにlocaleを含めない
+    I18n.locale == I18n.default_locale ? { locale: nil } : { locale: I18n.locale }
+  end
+
+  private
+
+  def switch_locale(&action)
+    locale = params[:locale] || I18n.default_locale
+    I18n.with_locale(locale, &action)
+  end
 end
---
+++ config/initializers/locale.rb
@@ -0,0 +1,4 @@
+# frozen_string_literal: true
+
+I18n.available_locales = %i[ja en]
+I18n.default_locale = :ja
