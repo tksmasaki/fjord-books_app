4e20113595776aa83703fbbfe28f2e74d9f0a6ee
tksmasaki <tk.masaki70@gmail.com>
2021-08-28 18:50:51 +0900

    アカウント登録・編集時に郵便番号、住所、自己紹介文を登録できる機能を追加

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,4 +2,12 @@
 
 class ApplicationController < ActionController::Base
   before_action :authenticate_user!
+  before_action :configure_permitted_parameters, if: :devise_controller?
+
+  protected
+
+  def configure_permitted_parameters
+    devise_parameter_sanitizer.permit(:sign_up, keys: %i[postcode address self_introduction])
+    devise_parameter_sanitizer.permit(:account_update, keys: %i[postcode address self_introduction])
+  end
 end
