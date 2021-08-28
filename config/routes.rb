b99ecab211811d281ff0c9fc9d86ae64a87dcfe3
tksmasaki <tk.masaki70@gmail.com>
2021-08-28 19:08:14 +0900

    gem 'letter_opener_web' を追加

--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,7 @@ Rails.application.routes.draw do
   devise_for :users, controllers: { registrations: 'users/registrations' }
   resources :users, only: %i[index show], controller: 'users/users'
   resources :books
+
+  mount LetterOpenerWeb::Engine, at: '/letter_opener' if Rails.env.development?
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
