daaf341e346000a8d1c0d71d34c5d3b7186030e1
tksmasaki <tk.masaki70@gmail.com>
2021-08-29 10:55:08 +0900

    ログイン、ログアウト後のリダイレクト先を変更

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,6 +4,17 @@ class ApplicationController < ActionController::Base
   before_action :authenticate_user!
   before_action :configure_permitted_parameters, if: :devise_controller?
 
+  def after_sign_in_path_for(resource)
+    stored_location_for(resource) || books_path
+  end
+
+  def after_sign_out_path_for(scope)
+    scope = Devise::Mapping.find_scope!(scope)
+    router_name = Devise.mappings[scope].router_name
+    context = router_name ? send(router_name) : self
+    context.respond_to?(:root_path) ? context.root_path : new_session_path(scope)
+  end
+
   protected
 
   def configure_permitted_parameters
