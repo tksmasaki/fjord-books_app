5e1038b6c03c26d2b8fc1d78c17befd8a260794e
tksmasaki <tk.masaki70@gmail.com>
2021-09-23 16:55:07 +0900

    Reportクラスのインスタンスメソッドに対するテストを追加

--- test/models/report_test.rb
+++ test/models/report_test.rb
@@ -3,7 +3,21 @@
 require 'test_helper'
 
 class ReportTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  def setup
+    @alice = users(:alice)
+    @bob = users(:bob)
+    @alice_report = reports(:alice_report)
+  end
+
+  test 'should #editable?(target_user) return true when #user equal target_user' do
+    assert @alice_report.editable?(@alice)
+  end
+
+  test 'should #editable?(target_user) return false when #user differ from target_user' do
+    assert_not @alice_report.editable?(@bob)
+  end
+
+  test 'should #created_on return Date object' do
+    assert_instance_of(Date, @alice_report.created_on)
+  end
 end
