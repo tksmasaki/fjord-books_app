ca9259114ffc1a69e3cb7905c09eb6094e60ab50
tksmasaki <tk.masaki70@gmail.com>
2021-09-23 16:24:57 +0900

    Userクラスのインスタンスメソッドに対するテストを追加

--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -3,7 +3,48 @@
 require 'test_helper'
 
 class UserTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  def setup
+    @alice = users(:alice)
+    @bob = users(:bob)
+  end
+
+  test 'should #following?(other) return true when user is following other' do
+    assert @alice.following?(@bob)
+  end
+
+  test 'should #following?(other) return false when user is not following other' do
+    assert_not @bob.following?(@alice)
+  end
+
+  test 'should #followed_by?(other) return true when user is followed by other' do
+    assert @bob.followed_by?(@alice)
+  end
+
+  test 'should #followed_by?(other) return false when user is not followed by other' do
+    assert_not @alice.followed_by?(@bob)
+  end
+
+  test 'can follow a user' do
+    assert_not @bob.following?(@alice)
+    @bob.follow @alice
+    assert @bob.following?(@alice)
+  end
+
+  test 'can unfollow a user' do
+    assert @alice.following?(@bob)
+    @alice.unfollow @bob
+    assert_not @alice.following?(@bob)
+  end
+
+  test 'should #name_or_email return name when name is present' do
+    assert @alice.name.present?
+    assert_equal 'Alice', @alice.name_or_email
+  end
+
+  test 'should #name_or_email return email when name is blank' do
+    ['', ' ', nil].each do |name|
+      @alice.name = name
+      assert_equal 'alice@example.com', @alice.name_or_email
+    end
+  end
 end
